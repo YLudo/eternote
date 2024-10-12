3e53bb238a5fde7d8792d5cfedc040a1b774dcb4
DevLudo <147008535+YLudo@users.noreply.github.com>
2024-10-12 22:08:44 +0200

    feat: add dashboard layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -8,7 +8,9 @@ export default function DashboardLayout({
     return (
         <div className="flex min-h-screen w-full flex-col">
             <Navbar />
-            {children}
+            <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col bg-zinc-50 dark:bg-zinc-900 gap-4 p-4 md:gap-8 md:p-10">
+                {children}
+            </main>
         </div>
     )
 }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,7 @@
 export default function DashboardPage() {
     return (
-        <></>
+        <>
+            <h1 className="text-2xl font-semibold">Tableau de bord</h1>
+        </>
     )
 }
