eec4bd45b4e8d81369fcc576cb80a01fa0221da8
DevLudo <147008535+YLudo@users.noreply.github.com>
2024-10-12 22:28:28 +0200

    fix: add links list to avoid same code

--- src/components/dashboard/navigation/NavbarMenu.tsx
+++ src/components/dashboard/navigation/NavbarMenu.tsx
@@ -2,6 +2,11 @@ import Link from "next/link";
 import { Timer } from "lucide-react";
 
 export default function NavbarMenu() {
+    const links = [
+        { href: "/dashboard", label: "Tableau de bord", },
+        { href: "/capsules/create", label: "Créer une capsule", },
+    ];
+
     return (
         <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
             <Link
@@ -11,12 +16,15 @@ export default function NavbarMenu() {
                 <Timer className="h-6 w-6" />
                 <span className="sr-only">Eternote</span>
             </Link>
-            <Link
-                href="/dashboard"
-                className="text-muted-foreground transition-colors hover:text-foreground"
-            >
-                Tableau de bord
-            </Link>
+            {links.map((link) => (
+                <Link
+                    key={link.href}
+                    href={link.href}
+                    className="text-muted-foreground transition-colors hover:text-foreground"
+                >
+                    {link.label}
+                </Link>
+            ))}
         </nav>
     );
 }
